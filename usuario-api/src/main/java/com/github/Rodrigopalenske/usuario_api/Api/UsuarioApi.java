aaa8a23a25a6f95e4f5c05cf2fd699c82b584b94
Rodrigo Palenske <rodrigopalenske@gmail.com>
2025-07-15 22:36:50 -0300

    Criando Api para cadastro de usuários
    Criando Api para cadastro de usuários para consumo no frontend

---
+++ usuario-api/src/main/java/com/github/Rodrigopalenske/usuario_api/Api/UsuarioApi.java
@@ -0,0 +1,28 @@
+package com.github.Rodrigopalenske.usuario_api.Api;
+
+import com.github.Rodrigopalenske.usuario_api.model.Usuario;
+import com.github.Rodrigopalenske.usuario_api.service.UsuarioService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/usuario")
+public class UsuarioApi {
+
+    private final UsuarioService service;
+
+    @PostMapping
+    public ResponseEntity salvar(@RequestBody Usuario usuario) {
+        try {
+            service.salvar(usuario);
+            return ResponseEntity.status(201).build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
